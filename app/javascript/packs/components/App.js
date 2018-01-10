7220a92d491127d49b6af89ccc768e1c5b17eb28
Piotr Leniec <leniec.piotr@gmail.com>
2018-01-10 22:01:09 +0100

    very simple layout

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import UploadButton from './UploadButton'
+import ProgressBar from './ProgressBar'
 import './App.scss'
 
 export default () => (
-  <h1>App Component</h1>
+  <div className="row">
+    <div className="col-xs-offset-4 col-xs-4">
+      <div className="panel panel-primary">
+        <div className="panel-body">
+          <UploadButton />
+          <ProgressBar />
+        </div>
+      </div>
+    </div>
+  </div>
 )
---
+++ app/javascript/packs/components/ProgressBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div className="progress">
+    <div className="progress-bar progress-bar-striped active" style={{ width: '45%' }}>
+    </div>
+  </div>
+)
