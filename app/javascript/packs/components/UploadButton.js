cab2a4c6da00cb226e00bbc9f8875cae7d89c9f9
Piotr Leniec <leniec.piotr@gmail.com>
2018-01-10 22:35:27 +0100

    splitting a file to chunks

--- app/javascript/packs/components/UploadButton.js
+++ app/javascript/packs/components/UploadButton.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
-export default () => (
-  <input type="file" />
-)
+const CHUNK_SIZE = 1000000
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.processFile = this.processFile.bind(this)
+  }
+
+  processFile(event) {
+    this.uploadChunk(event.target.files[0], 0)
+  }
+
+  uploadChunk(file, chunkIndex) {
+    const fileSlice = file.slice(chunkIndex * CHUNK_SIZE, (chunkIndex + 1) * CHUNK_SIZE)
+    if (fileSlice.size === 0) return
+
+    const fileReader = new FileReader()
+    fileReader.onload = event => {
+      const arrayBuffer = event.target.result
+      console.log('CHUNK INDEX', chunkIndex, 'CHUNK LENGTH', arrayBuffer.byteLength)
+      this.uploadChunk(file, chunkIndex + 1)
+    }
+    fileReader.readAsArrayBuffer(fileSlice)
+  }
+
+  render() {
+    return (
+      <input type="file" onChange={this.processFile} />
+    )
+  }
+}
