246d8bb9e646d4aaebac577e7b57fc49843bb170
Piotr Leniec <leniec.piotr@gmail.com>
2018-01-10 23:17:09 +0100

    integrated ProgressBar with redux

--- app/javascript/packs/components/ProgressBar.js
+++ app/javascript/packs/components/ProgressBar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { CHUNK_SIZE } from '../constants'
 
-export default () => (
+const ProgressBar = props => (
   <div className="progress">
-    <div className="progress-bar progress-bar-striped active" style={{ width: '45%' }}>
+    <div
+      className="progress-bar progress-bar-striped active"
+      style={{ width: `${props.percentage}%` }}
+    >
     </div>
   </div>
 )
+
+const mapStateToProps = state => {
+  const { fileSize, chunkIndex } = state.chunkedUpload
+
+  return { percentage: 100 * fileSize / (chunkIndex * CHUNK_SIZE) }
+}
+
+export default connect(mapStateToProps)(ProgressBar)
