6987f6f76f147c45d2aa2a8cb75ad8f45e6df369
Piotr Leniec <leniec.piotr@gmail.com>
2018-01-11 20:32:27 +0100

    fixed code style

--- app/models/chunked_upload_task.rb
+++ app/models/chunked_upload_task.rb
@@ -1,5 +1,7 @@
 class ChunkedUploadTask < ApplicationRecord
   has_many :file_chunks, dependent: :destroy
 
-  validates :file_size, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :file_size, presence: true, numericality: {
+    only_integer: true, greater_than: 0
+  }
 end
